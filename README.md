f31196c8c7039527d82437e87ce8bf721a55d0d6
df4637816 <df4637816@yahoo.com.tw>
2024-10-13 21:42:57 +0800

    new repo

---
+++ README.md
@@ -0,0 +1,18 @@
+# Create T3 App
+
+## T3 project 01
+
+### Todo
+
+- [x] 部署網頁(vercel)
+- [X] 架設基礎 ui和 mock data
+- [X] Tidy up build process 
+- [] 確實建立一個database
+- [] 附上database到ui
+- [] 加入權限authentication
+- [] 上傳圖片
+- [] error management
+- [] routing/圖片頁面
+- [] delete button
+- [] Analytics(posthog)
+- [] Ratelimiting(upstash)
---
+++ src/app/page.tsx
@@ -0,0 +1,32 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import Link from "next/link";
+import Image from "next/image";
+const mockUrl = [
+  "https://utfs.io/f/a44207b0-f7bf-4af1-9fc2-408a0420ea9b-2xgg1h.png",
+  "https://utfs.io/f/3d50bf61-0bc6-43fb-85ee-1319608a585e-erwkck.JPG",
+  "https://utfs.io/f/fbe7f54d-a3f4-4dce-90e3-d3405d5a84e0-y8inzg.JPG",
+  "https://utfs.io/f/5169f794-e920-4a6a-b82e-a09f61803a16-6b64nn.JPG",
+]
+
+const mocklImages = mockUrl.map((url:string,index:number) => (
+   {
+    id:index + 1,
+    url
+   }
+))
+
+export default function HomePage() {
+  return (
+    <main className="w-full flex justify-center items-center">
+    <div className="flex flex-wrap justify-center gap-2">
+      {
+        [...mocklImages,...mocklImages,...mocklImages].map((images)=>(
+            <div className="" key={images.id}>
+              <Image  src={images.url} alt={"image"} width={250} height={100}    style={{objectFit: "cover",aspectRatio:'1/1'}}/>
+           </div>
+        ))
+      }
+    </div>
+    </main>
+  );
+}
