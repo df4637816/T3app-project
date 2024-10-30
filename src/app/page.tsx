d5ea7cf47c33e655c1c6c4979e63c66225364570
df4637816 <df4637816@yahoo.com.tw>
2024-10-30 13:27:58 +0800

    Database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import Link from "next/link";
 import Image from "next/image";
+import { db } from "~/server/db";
+
+export const dynamic = "force-dynamic";
 const mockUrl = [
   "https://utfs.io/f/a44207b0-f7bf-4af1-9fc2-408a0420ea9b-2xgg1h.png",
   "https://utfs.io/f/3d50bf61-0bc6-43fb-85ee-1319608a585e-erwkck.JPG",
@@ -15,14 +18,18 @@ const mocklImages = mockUrl.map((url:string,index:number) => (
    }
 ))
 
-export default function HomePage() {
+export default async function HomePage() {
+
+  const posts = await db.query.posts.findMany();
+
   return (
     <main className="w-full flex justify-center items-center">
     <div className="flex flex-wrap justify-center gap-2">
+      {posts.map((post)=> (<div key={post.id}>{post.name}</div>))}
       {
-        [...mocklImages,...mocklImages,...mocklImages].map((images)=>(
-            <div className="" key={images.id}>
-              <Image  src={images.url} alt={"image"} width={250} height={100}    style={{objectFit: "cover",aspectRatio:'1/1'}}/>
+        [...mocklImages,...mocklImages,...mocklImages].map((images,index)=>(
+            <div className="" key={images.id + "-" + index}>
+              <Image   src={images.url} alt={"image"} width={250} height={100}    style={{objectFit: "cover",aspectRatio:'1/1'}}/>
            </div>
         ))
       }
